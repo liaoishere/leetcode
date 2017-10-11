4ca146408758881ed00f93de402e0e66af0ed189
Penghui Liao <liaoishere@gmail.com>
2017-10-12 00:33:56 +0800

    Add two sum solution
    Signed-off-by: Penghui Liao <liaoishere@gmail.com>

---
+++ problems/two_sum/two_sum.go
@@ -0,0 +1,14 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int, len(nums))
+	for i, v := range nums {
+		if c, ok := m[target-v]; ok {
+			return []int{c, i}
+		}
+		if _, ok := m[v]; !ok {
+			m[v] = i
+		}
+	}
+	return []int{}
+}
